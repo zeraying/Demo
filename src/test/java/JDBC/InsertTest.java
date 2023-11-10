ad5fae8d8c4f9313b341a7fa3390975d4812de4b
彭振宇 <3426136249@qq.com>
2023-11-10 20:00:54 +0800

    变更

--- src/test/java/JDBC/InsertTest.java
+++ src/test/java/JDBC/InsertTest.java
@@ -1,5 +1,10 @@
 package JDBC;
 
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /**
  * @author pzy
  * @create 2023-11-10-14:17
@@ -7,4 +12,30 @@ package JDBC;
  * PrepareStatement实现数据的批量操作
  */
 public class InsertTest {
+//    在MySQL配置文件url后面添加?rewriteBatchedStatements=true开启MySQL的批处理操作
+    @Test
+    public void insertdate(){
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            long start = System.currentTimeMillis();
+            connection = JDBCUtils.getConnection();
+            String sql = "insert into new_table(`id`) values (?)";
+            ps = connection.prepareStatement(sql);
+            for (int i = 0; i <= 100000; i++) {
+                ps.setObject(1,i);
+                ps.addBatch();
+                if (i % 500 == 0) {
+                    ps.executeBatch();
+                    ps.clearBatch();
+                }
+            }
+            long stop = System.currentTimeMillis();
+            System.out.println(stop - start);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtils.closeResource(connection,ps);
+        }
+    }
 }
